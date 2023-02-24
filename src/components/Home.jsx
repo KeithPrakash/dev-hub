5aae9e1abdeb48e243cf544c83cc945f3e8319aa
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2023-02-24 23:40:15 +0530

    2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
 import { useEffect,useState } from 'react';
-
-
 const Home = () => {
 const [blog, setBlogs] = useState(null);
 const [isPending,setIsPending]= useState(true);
+      
 
-
+let count = 0;
     useEffect(()=>{
           fetch('http://localhost:8000/blogs',{}).then( (res)=>{return res.json()}).then((data)=>{
             setIsPending(false);
@@ -16,15 +15,26 @@ const [isPending,setIsPending]= useState(true);
     },[])
     return (
       <div className="homeContainer">
-        <h1 style={{ color: "white" }}>Home page</h1>
-
         <div className="list">
           {blog
             ? blog.map((data) => (
                 <div className="preview" key={data.id}>
                   {" "}
-                  <p style={{ color: "white" }}>{data.title}</p>
-                  <a style={{ color: "yellow" }} href={data.link}>{data.link}</a>
+                  <div className="num">
+                    {" "}
+                    <p>{(count += 1)}</p>
+                  </div>
+                  <div className="story">
+                    {" "}
+                    <p style={{ color: "white" }}>{data.title}</p>
+                    <a style={{ color: "yellow" }} href={data.link}>
+                      {data.link}
+                    </a>{" "}
+                    <p>{data.date}</p>
+                  </div>
+                  <div className="like">
+        icon
+                  </div>
                 </div>
               ))
             : null}
