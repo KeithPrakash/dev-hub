92e596628d31fed426d9d325f44e7415b0267b9a
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2023-02-27 21:13:06 +0530

    2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
+import { Grid } from '@mui/material';
 import { useEffect,useState } from 'react';
+import Icon from "@mui/material/Icon";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 const Home = () => {
 const [blog, setBlogs] = useState(null);
 const [isPending,setIsPending]= useState(true);
-      
+
 
 let count = 0;
     useEffect(()=>{
@@ -18,24 +21,33 @@ let count = 0;
         <div className="list">
           {blog
             ? blog.map((data) => (
-                <div className="preview" key={data.id}>
-                  {" "}
-                  <div className="num">
-                    {" "}
-                    <p>{(count += 1)}</p>
-                  </div>
-                  <div className="story">
-                    {" "}
-                    <p style={{ color: "white" }}>{data.title}</p>
-                    <a style={{ color: "yellow" }} href={data.link}>
-                      {data.link}
-                    </a>{" "}
-                    <p>{data.date}</p>
-                  </div>
-                  <div className="like">
-        icon
-                  </div>
-                </div>
+                <Grid container>
+                  <Grid item xs={12}>
+                    <div className="preview" key={data.id}>
+                      <div className="num">
+                        <p>{(count += 1)}</p>
+                      </div>
+                      <div className="story">
+                        {" "}
+                        <p style={{ color: "white" }}>{data.title}</p>
+                        <a
+                          style={{
+                            color: "#464AB5",
+                            fontSize: 12,
+                            hover: "green",
+                          }}
+                          href={data.link}
+                        >
+                          Read Me
+                        </a>{" "}
+                        <p style={{ fontSize: 12 }}>{data.date}</p>
+                      </div>
+                      <div className="like">
+                        <ChatBubbleOutlineIcon />
+                      </div>
+                    </div>
+                  </Grid>
+                </Grid>
               ))
             : null}
         </div>
